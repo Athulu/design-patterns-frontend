3ccc97c729f089a3a15b7daaea5fa2577a05d3c4
Nikodem Wójciak <nikodem1703@gmail.com>
2022-12-16 13:15:51 +0100

    checkpoint

--- src/app/components/fabrykamd/fabrykamd.component.ts
+++ src/app/components/fabrykamd/fabrykamd.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {DesignPatternsClientService} from "../../services/design-patterns-client.service";
 
 @Component({
   selector: 'app-fabrykamd',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FabrykamdComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  file: any;
+
+  constructor(private httpClient:HttpClient, private designPatternsService: DesignPatternsClientService) { }
 
   ngOnInit(): void {
   }
 
+  getFile(event:any){
+    this.file = event.target.files[0];
+    console.log('file', this.file);
+  }
+
+  submitData(){
+    let formData = new FormData();
+    formData.set("name", this.name);
+    formData.set("file", this.file);
+
+    this.httpClient.post('http://localhost:8080/testing/2', formData)
+      .subscribe((response) =>{});
+  }
 }
