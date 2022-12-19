634b713e8286c432de1499de955d5f0d027dac2b
Nikodem Wójciak <nikodem1703@gmail.com>
2022-12-19 03:15:28 +0100

    skończona fabryka abstrakcyjna

--- src/app/components/fabrykaab/fabrykaab.component.ts
+++ src/app/components/fabrykaab/fabrykaab.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ResultObject} from "../../interfaces/result-object";
+import {HttpClient} from "@angular/common/http";
+import {DesignPatternsClientService} from "../../services/design-patterns-client.service";
+import {CookieService} from "../../services/cookie.service";
 
 @Component({
   selector: 'app-fabrykaab',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FabrykaabComponent implements OnInit {
 
-  constructor() { }
+  public results: ResultObject[] = [];
+  name: string = '';
+  file: any;
+
+  constructor(private httpClient:HttpClient, private designPatternsService: DesignPatternsClientService) { }
 
   ngOnInit(): void {
+    this.designPatternsService.getResultsList("fabrykaab").subscribe((results) => {
+      this.results = results;
+    });
+  }
+
+  IsListEmpty(): boolean{
+    if(this.results.length < 1){
+      return false;
+    }else {
+      return true;
+    }
+  }
+
+  getFile(event:any){
+    this.file = event.target.files[0];
+    console.log('file', this.file);
+  }
+
+  submitData(){
+    const cookie = new CookieService();
+    sessionStorage.setItem("fabrykaab", cookie.cookie);
+
+    let formData = new FormData();
+    formData.set("name", this.name);
+    formData.set("file", this.file);
+    // @ts-ignore
+    formData.set("cookie", sessionStorage.getItem("fabrykaab")); //cookie.cookie
+
+    this.httpClient.post('http://localhost:8080/testing/3', formData)
+      .subscribe((response) =>{});
   }
 
 }
